8c6c8feba708b52c53129b1c8804e5ad5ea79936
pilarhugo-debug <pilar.hugo@estudiantes.utec.edu.uy>
2026-05-22 00:22:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,23 +10,14 @@ navigator.mediaDevices.getUserMedia({
 });
 
 snap.addEventListener("click", () => {
-
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
 
   const ctx = canvas.getContext("2d");
-
-  // limpiar canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // espejo REAL
-  ctx.translate(canvas.width, 0);
-  ctx.scale(-1, 1);
-
-  // dibujar
+  // Dibujar directo, sin espejo
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-  // resetear transformación
-  ctx.setTransform(1, 0, 0, 1, 0, 0);
-
 });
+
+
